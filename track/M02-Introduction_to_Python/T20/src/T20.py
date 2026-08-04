959f4d1adbb09bce88a5ab64de38f707a61b91c5
Billy Daniel W <danieldany068@gmail.com>
2026-08-04 17:31:54 +0530

    04/08

---
+++ track/M02-Introduction_to_Python/T20/src/T20.py
@@ -0,0 +1,11 @@
+student_name = input()
+student_age = int(input())
+course_rating = int(input())
+
+print(f"Student Name: {student_name} ")
+print(f"Age: {student_age} ")
+print(f"Course Rating: {course_rating}")
+
+print(type(student_name))
+print(type(student_age))
+print(type(course_rating))
