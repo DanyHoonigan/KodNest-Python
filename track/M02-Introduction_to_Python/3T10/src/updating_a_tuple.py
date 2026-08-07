c6796d2237847809cc14ffd04b4b8b64d2a2dc4f
Billy Daniel W <danieldany068@gmail.com>
2026-08-07 16:06:41 +0530

    06/08 updating a tuple

---
+++ track/M02-Introduction_to_Python/3T10/src/updating_a_tuple.py
@@ -0,0 +1,11 @@
+course_name = input("Enter the course name: ")
+course_week = input("Enter the week of the course: ")
+course_status = input("Enter the status of the course: ")
+
+course_detail = (course_name,course_week,course_status)
+print(course_detail)
+
+updated_week = input("Enter the updated week: ")
+
+updated_course_detail = (course_name,updated_week,course_status)
+print(updated_course_detail)
