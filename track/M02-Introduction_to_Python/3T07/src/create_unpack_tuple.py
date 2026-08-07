32c0db020c96b3b247b8fbc52c7b47949013580f
Billy Daniel W <danieldany068@gmail.com>
2026-08-07 15:41:33 +0530

    06/08 unpacking a tuple

---
+++ track/M02-Introduction_to_Python/3T07/src/create_unpack_tuple.py
@@ -0,0 +1,8 @@
+name = input("Enter your name: ")
+course = input("Enter the course you have chosen: ")
+score = int(input("Enter your score: "))
+Tuple = (name,course,score)
+a,b,c = Tuple
+print(f"Name: {a}")
+print(f"Course: {b}")
+print(f"Score: {c}")
