1d5111950b1d5e516534ebff171724e9d4dfdb37
Billy Daniel W <danieldany068@gmail.com>
2026-08-07 16:18:16 +0530

    changed my mistake (I have used the variables directly as it is instead of just taking the index value from old tuple and I have added my mistake as a comment

--- track/M02-Introduction_to_Python/3T10/src/updating_a_tuple.py
+++ track/M02-Introduction_to_Python/3T10/src/updating_a_tuple.py
@@ -7,5 +7,6 @@ print(course_detail)
 
 updated_week = input("Enter the updated week: ")
 
-updated_course_detail = (course_name,updated_week,course_status)
+#updated_course_detail = (course_name,updated_week,course_status)
+updated_course_detail = (course_detail[0],updated_week,course_detail[2])
 print(updated_course_detail)
