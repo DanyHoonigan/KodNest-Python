46449de2f74b13569ddf5d75dda50ff3c9f860f1
Billy Daniel W <danieldany068@gmail.com>
2026-08-20 00:11:26 +0530

    using a function a student profile is updated multiple times

---
+++ track/M02-Introduction_to_Python/4T11/src/Students_detail_using_function.py
@@ -0,0 +1,11 @@
+def display_student(name, course, level):
+    print(f"{name} | {course} | {level}")
+
+# Subtask 2: Positional arguments for Aarav
+display_student("Aarav", "Python", "Beginner")
+
+# Subtask 3: Keyword arguments for Meera
+display_student(name="Meera", course="Java", level="Intermediate")
+
+# Subtask 4: Mixed arguments (positional + keyword) for Kabir
+display_student("Kabir", course="SQL", level="Beginner")
